279437d66a4220c6f81fb9bac477a9a763bbe7c7
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-10-01 14:07:03 +0200

    add data validation

--- infrastructure/Models/Box.cs
+++ infrastructure/Models/Box.cs
@@ -1,11 +1,20 @@
-﻿namespace infrastructure.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace infrastructure.Models;
 
 public class Box
 {
     public Guid? Guid { get; set; }
+    [Required]
+    [Range(0, 1000)]
     public decimal? Width { get; set; }
+    [Required]
+    [Range(0, 1000)]
     public decimal? Height { get; set; }
+    [Required]
+    [Range(0, 1000)]
     public decimal? Depth { get; set; }
+    [Required]
     public string? Location { get; set; }
     public string? Description { get; set; }
     public DateTime? Created { get; set; }
